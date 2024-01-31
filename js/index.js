2a94688ac2aaea4a14e874a5cb37685309018716
Cl0wnFiesta <clownfiesta98@gmail.com>
2024-01-31 17:35:55 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// js/index.js
+import('../pkg/index.js').then(module => {
+    const { add } = module;
+    
+    document.getElementById('button').addEventListener('click', function () {
+        const inputNumber = document.getElementById('inputNumber').valueAsNumber;
+        const result = add(inputNumber, 3); // Use inputNumber in the add function
+        document.getElementById('result').textContent = `Result: ${result}`;
+    });
+});
